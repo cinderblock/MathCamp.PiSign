f352ea8be351933cd80b3032d149ca18f0f4c9c8
Cameron Tacklind <cameron@tacklind.com>
2019-06-25 22:52:41 -0700

    Import old WS281x control protocol

--- daemon/main.ts
+++ daemon/main.ts
@@ -4,6 +4,25 @@
 import * as debug from './utils/debug';
 import makeClientHandler from './ClientHandler';
 
+import WS2812 from 'rpi-ws281x-native';
+
+debug.green('Hello, world.');
+
+const numLeds = pixels.length;
+
+WS2812.init(numLeds);
+WS2812.setBrightness(255);
+
+const buff = new Uint32Array(numLeds);
+
+setInterval(() => {
+  const frame = { pixels: animation(Date.now()) };
+
+  buff.set(frame.pixels.map(({ r, g, b }) => (r << 16) | (g << 8) | b));
+
+  WS2812.render(buff);
+}, 1000 / 40);
+
 // Events from the clients and how to handle them
 const remoteControlServer = makeClientHandler({
   // This event happens when mobile devices report their orientation data to the server.
